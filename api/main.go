0cd4d265c69824aae8609c73e97f3249cc0a709a
joaopmjm <joao.meirelles@stone.com.br>
2021-06-14 16:16:11 -0300

    Simpler API

--- api/main.go
+++ api/main.go
@@ -1,21 +1,64 @@
 package main
 
 import (
-    "fmt"
+    "encoding/json"
+    "github.com/gorilla/mux"
     "log"
+	"time"
     "net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+type Todo struct {
+    Id           string    `json:"id,omitempty"`
+    Title        string    `json:"firstname,omitempty"`
+    Description  string    `json:"lastname,omitempty"`
+    Updated      time.Time `json:"address,omitempty"`
 }
 
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+var todos []Todo
+
+func GetTodos(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode(todos)
+}
+
+func GetTodo(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for _, item := range todos {
+        if item.Id == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
+        }
+    }
+    json.NewEncoder(w).Encode(&Todo{})
+}
+
+func NewTodo(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    var newTodo Todo
+    _ = json.NewDecoder(r.Body).Decode(&newTodo)
+    newTodo.Id = params["id"]
+    todos = append(todos, newTodo)
+    json.NewEncoder(w).Encode(todos)
+}
+
+func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for index, item := range todos {
+        if item.Id == params["id"] {
+            todos = append(todos[:index], todos[index+1:]...)
+            break
+        }
+        json.NewEncoder(w).Encode(todos)
+    }
 }
 
 func main() {
-    handleRequests()
+    router := mux.NewRouter()    
+	todos = append(todos, Todo{Id: "1", Title: "Sei la mano", Description: "Todos malucas", Updated: time.Now()})
+	todos = append(todos, Todo{Id: "2", Title: "Louco mesmo", Description: "Todos loucas", Updated: time.Now()})
+	router.HandleFunc("/todos", GetTodos).Methods("GET")
+    router.HandleFunc("/todos/{id}", GetTodo).Methods("GET")
+    router.HandleFunc("/todos/{id}", NewTodo).Methods("POST")
+    router.HandleFunc("/todos/{id}", DeleteTodo).Methods("DELETE")    
+	log.Fatal(http.ListenAndServe(":8001", router))
 }
--- api/routes/ping.go
+++
@@ -1,8 +0,0 @@
-package routes
-
-import "github.com/gofiber/fiber"
-
-// Ping : testing endpoint.
-func Ping(c *fiber.Ctx) error {
-	return c.SendString("Pong!")
-}
